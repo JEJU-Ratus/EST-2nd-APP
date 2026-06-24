5c745192af5fda052b6a35c51880f2f9e7ffd61c
jema <smh1141@naver.com>
2026-06-24 16:19:07 +0900

    feat: MyArticle 및 UpdateArticle 컴포넌트 수정 버튼 및 삭제 버튼 조건부 렌더링 추가

--- src/components/UpdateArticel.jsx
+++ src/components/UpdateArticel.jsx
@@ -2,13 +2,27 @@ import { useState } from "react";
 
 export default function UpdateArticle({ title, desc, onSubmit }) {
   console.log("UpdateArticle render");
-  const [newTitle, setNewTitle] = useState(title);
-  const [newDesc, setNewDesc] = useState(desc);
-  const handleTitleChange = e => {
-    setNewTitle(e.target.value);
-  };
-  const handleDescChange = e => {
-    setNewDesc(e.target.value);
+  // const [newTitle, setNewTitle] = useState(title);
+  // const [newDesc, setNewDesc] = useState(desc);
+  // const handleTitleChange = e => {
+  //   setNewTitle(e.target.value);
+  // };
+  // const handleDescChange = e => {
+  //   setNewDesc(e.target.value);
+  // };
+  const [content, setContent] = useState({
+    title: title,
+    desc: desc,
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setContent(prev => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   };
   return (
     <>
@@ -17,7 +31,7 @@ export default function UpdateArticle({ title, desc, onSubmit }) {
         action=""
         onSubmit={e => {
           e.preventDefault();
-          onSubmit(newTitle, newDesc);
+          onSubmit(content.title, content.desc);
         }}
       >
         <div>
@@ -26,13 +40,13 @@ export default function UpdateArticle({ title, desc, onSubmit }) {
             type="text"
             id="title"
             name="title"
-            value={newTitle}
-            onChange={handleTitleChange}
+            value={content.title}
+            onChange={handleChange}
           />
         </div>
         <div>
           <label htmlFor="desc">desc</label>
-          <textarea name="desc" id="desc" value={newDesc} onChange={handleDescChange}></textarea>
+          <textarea name="desc" id="desc" value={content.desc} onChange={handleChange}></textarea>
         </div>
         <button>submit</button>
       </form>
